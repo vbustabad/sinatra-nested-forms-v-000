6ddd9856d4f5ad66554ce6ced445a42ed34c0491
Veronica Bustabad <vbustabad@gmail.com>
2018-07-12 19:48:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,25 +4,25 @@ module FormsLab
 
   class App < Sinatra::Base
 
-  get '/' do
-    "Welcome to the Nested Forms Lab!"
-    "Let's navigate to the '/new'"
-  end
-
-  get '/new' do
-    erb :new
-  end
-
-  post '/pirates' do
-    @pirate = Pirate.new(params[:pirate])
-
-    params[:pirate][:ships].each do |details|
-      Ship.new(details)
+    get '/' do
+      "Welcome to the Nested Forms Lab!"
+      "Let's navigate to the '/new'"
     end
 
-    @@ships = Ship.all
+    get '/new' do
+      erb :new
+    end
 
-    erb :show
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @@ships = Ship.all
+
+      erb :show
+    end
+
   end
-
-end
